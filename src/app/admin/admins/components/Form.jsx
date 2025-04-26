316b994805f3c933c7e96a42ac7b8040fe24e9bc
Sandip <sandipghimire254@gmail.com>
2025-04-26 22:35:09 +0545

    updating the admins

---
+++ src/app/admin/admins/components/Form.jsx
@@ -0,0 +1,99 @@
+"use client"
+import { useState } from "react"
+import toast from "react-hot-toast";
+
+export default function Form() {
+    const [name, setName] = useState("");
+    const [email, setemail] = useState("");
+    const [image, setImages] = useState(null);
+
+    const handleSubmit = async () => {
+        try {
+            const formData = new FormData();
+            formData.append("name", name);
+            formData.append("email", email);
+            formData.append("image", image);
+
+            const res = await fetch("/api/admins", {
+                method: "POST",
+                body: formData,
+            });
+
+            const result = await res.json();
+            if (res.ok) {
+                console.log("✅ Category created:", result);
+                toast.success("admins Created Successfully!!")
+                setImages(null)
+                setName(null)
+                setemail(null)
+            } else {
+                toast.error("Fill all the fields.")
+                console.error("❌ Error:", result.message);
+            }
+        } catch (err) {
+            console.error("Fetch error:", err);
+        }
+    };
+
+    return <div className="bg-white rounded-lg p-5 w-ful md:w-[400px]">
+        <h1 className="font-semibold pb-2 flex justify-center items-center">Create admins</h1>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                handleSubmit();
+            }}
+            className="flex flex-col gap-5"
+        >
+            <div className="flex flex-col gap-1">
+                <label className="text-slate-600">
+                    Image <span className="text-red-600">*</span>
+                </label>
+                {image && (
+                    <img
+                        src={URL.createObjectURL(image)}
+                        alt="Preview"
+                        className="w-32 h-32 object-cover rounded-md"
+                    />
+                )}
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={e => {
+                        const file = e.target.files?.[0];
+                        if (file) setImages(file);
+                    }}
+                    className="border px-4 py-2 rounded-lg focus:outline-none"
+                />
+            </div>
+            <div className="flex flex-col gap-1">
+                <label className="text-slate-600">
+                    Name <span className="text-red-600">*</span>
+                </label>
+                <input
+                    type="text"
+                    placeholder="Enter category name"
+                    id="category-name"
+                    name="category-name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+                />
+            </div>
+            <div className="flex flex-col gap-1">
+                <label className="text-slate-600">
+                    Email <span className="text-red-600">*</span>
+                </label>
+                <input
+                    type="email"
+                    placeholder="Enter email"
+                    id="admin-email"
+                    name="admin-email"
+                    value={email}
+                    onChange={(e) => setemail(e.target.value)}
+                    className="border px-4 py-2 rounded-lg w-full focus:outline-none"
+                />
+            </div>
+            <button className="flex justify-center items-center px-2 py-2 bg-blue-600 w-full rounded-lg text-white font-semibold">Create</button>
+        </form>
+    </div>
+}
